decd280de6f23bfee6040f79c4203a75b3cda9f8
Volodymyr Pasichnyi <vlpasnj33@gmail.com>
2023-02-07 18:21:00 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { ContactForm } from "./ContactForm/ContactForm";
+import { ContactList } from "./ContactsList/ContactList";
+import { Filter } from "./Filter/Filter";
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+      <div>
+        <h2>Phonebook</h2>
+        <ContactForm />
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+      </div>
+  )
+}
+
+
+
